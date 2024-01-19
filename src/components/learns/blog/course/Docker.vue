d1fcffdd39a886e9ccd4e898cea099d98c65f8f2
hieuvd99 <113vuonghieu555@gmail.com>
2024-01-19 17:08:11 +0700

    Coding #20210118_Blog

--- src/components/learns/blog/course/Docker.vue
+++ src/components/learns/blog/course/Docker.vue
@@ -1,11 +1,38 @@
 <template>
     <div class="content-course">
         <h1 class="title-content-course">Docker</h1>
+        <p class="text-content">Docker dùng để build, deploy, running ứng dụng dễ dàng hơn bằng cách sử dụng các container.</p>
+
+        <h5 class="title-child">Khi nào sử dụng Docker:</h5>
+        <div class="text-content">
+            <ul>
+                <li>Triển khai kiến trúc Microservices.</li>
+                <li>Khi xây dựng ứng dụng và cần scale một cách linh hoạt.</li>
+                <li>Giảm thời gian và công sức để config máy ở local và server.</li>
+                <li>Code một chỗ chạy nhiều nơi.</li>
+            </ul>
+        </div>
+
+        <h5 class="title-child">Một số khái niệm:</h5>
+        <div class="text-content">
+            <h6 class="title-child">Dockerfile</h6>
+            <p>Là một file config hướng dẫn cách để build ra images</p>
+
+            <h6 class="title-child">Images</h6>
+            <p>Run ra Container </p>
+
+            <h6 class="title-child">Container</h6>
+            <p>Được đóng gói, chạy được ở nhiều nơi</p>
+        </div>
 
 
-
-
-        
+        <h5 class="title-child">Tham khảo</h5>
+        <div class="text-content">
+            <ul>
+                <li><a href="https://www.youtube.com/watch?v=1k8pox8mkxc" class="link-refer">Tự học Docker siêu tốc trong 10 phút (Có code mẫu và ví dụ) - Code Dạo Dễ Òm</a></li>
+                <li><a href="https://viblo.asia/p/docker-la-gi-kien-thuc-co-ban-ve-docker-maGK7qeelj2" class="link-refer">Docker là gì? Kiến thức cơ bản về Docker</a></li>
+            </ul>
+        </div>
     </div>
 </template>
 
